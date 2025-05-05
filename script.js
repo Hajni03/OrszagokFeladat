74cdc7aa3afdb4879eacda9fed58dc4e924364b5
Hajni03 <dobi.hajnalka@simonyiszki.org>
2025-05-05 11:09:41 +0200

    kártyák megjelenítése

--- script.js
+++ script.js
@@ -6,5 +6,29 @@ fetch('https://api.sampleapis.com/countries/countries')
             countries[`country_${country.id}`] = country;
         });
         console.log(countries);
+        loadCards(countries);
     })
     .catch(error => console.log('Hiba: ', error));
+
+function loadCards(countries){
+    const cardContainer = document.getElementById('card-container');
+
+    if(!countries || Object.keys(countries).length === 0){
+        console.error('Hiba: Nincsenek országok');
+        return;
+    }
+
+    for(const key in countries){
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+        <h2>${countries[key].name}</h2>
+        <img src="${countries[key].media.flag}" alt="${countries[key].name}_zaszlo">
+        <p><strong>Főváros:</strong>${countries[key].capital}</p>
+        <p><strong>Pénznem:</strong>${countries[key].currency}</p>
+        <p><strong>Rövidítés</strong> ${countries[key].abbrevation}</p>
+        `;
+
+        cardContainer.appendChild(card);
+    }
+}
